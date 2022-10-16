26ffb41fadd7fb5f2b508e64c3545bc67a917080
Tharindu shakya <73313576+tharindu-hub@users.noreply.github.com>
2022-10-16 23:20:00 +0530

    Create main.cpp
    this is an ui design example

---
+++ mainui.cpp
@@ -0,0 +1,34 @@
+	void show_welcome() {
+		while(m_usb.isConnected()) {
+			m_con.clear();
+			m_con << "****************************************" << endl;
+			m_con << "*     Welcome to Self-Balance Robot    *" << endl;
+			m_con << "*                 " VERSION "               *" << endl;
+			m_con << "****************************************" << endl;
+			m_con << "[1] Calibrations" << endl;
+			m_con << "[2] PID Control tuning" << endl;
+			m_con << "[3] Save changed" << endl;
+			m_con << "[4] Load Default" << endl;
+			m_con << "[5] Dashboard" << endl;
+			switch(m_con.getc()) {
+			case '1' :
+				show_mpu6050();
+				break;
+			case '2' :
+				show_pid();
+				break;
+			case '3' :
+				saveConfigure();
+				break;
+			case '4' :
+				setDefault();
+				break;
+			case '5':
+				dashboard();
+				break;
+			case 'H':
+				m_con.printf("High-Water Mark:%d\n", getStackHighWaterMark());
+				m_con.getc();
+				break;
+			}
+		}
